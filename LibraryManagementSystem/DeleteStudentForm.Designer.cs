31e68ecd22da0b9d571ca03b457d6d568aa34fee
oshafcai <123338493+oshafcai@users.noreply.github.com>
2024-05-16 19:05:20 +0300

    Add files via upload

---
+++ LibraryManagementSystem/DeleteStudentForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.searchbtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(412, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(376, 272);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(71, 212);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 32);
+            this.textBox1.TabIndex = 3;
+            // 
+            // searchbtn
+            // 
+            this.searchbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.searchbtn.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.searchbtn.Location = new System.Drawing.Point(287, 161);
+            this.searchbtn.Name = "searchbtn";
+            this.searchbtn.Size = new System.Drawing.Size(104, 36);
+            this.searchbtn.TabIndex = 4;
+            this.searchbtn.Text = "search";
+            this.searchbtn.UseVisualStyleBackColor = true;
+            this.searchbtn.Click += new System.EventHandler(this.searchbtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.deletebtn.Location = new System.Drawing.Point(287, 213);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(104, 31);
+            this.deletebtn.TabIndex = 5;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox2.Location = new System.Drawing.Point(71, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 32);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label3.Location = new System.Drawing.Point(66, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dlete Studnt Form";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.searchbtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button searchbtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LibraryManagementSystem/DeleteStudentForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        String sql = "Data Source=NADRASAAD\\SQLEXPRESS;Initial Catalog=LIBRARY_DB;Integrated Security=True";
+
+        SqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(sql); // Initialize the SqlConnection object
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = LoadUsertable();
+        }
+        public DataTable LoadUsertable()
+        {
+            DataTable dt = new DataTable();
+            string query = "SELECT * FROM [USER]";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+
+        private void searchbtn_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                DataTable dt = new DataTable();
+                string query = "SELECT * FROM [USER] WHERE USERNAME = @username";
+
+
+                // Using parameterized query to prevent SQL injection
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", textBox2.Text);
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                }
+                conn.Close();
+
+                // Checking if any rows are returned before binding to DataGridView
+                if (dt.Rows.Count > 0)
+                {
+                    dataGridView1.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show("No user found with the given user ID.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to delete this user?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        conn.Open();
+
+                        // Delete related records from STUDENT table
+                        string deleteStudentsQuery = "DELETE FROM STUDENT WHERE USER_ID = @userid";
+                        SqlCommand deleteStudentsCmd = new SqlCommand(deleteStudentsQuery, conn);
+                        deleteStudentsCmd.Parameters.AddWithValue("@userid", dataGridView1.CurrentRow.Cells["USER_ID"].Value.ToString());
+                        deleteStudentsCmd.ExecuteNonQuery();
+
+                        // Delete record from USER table
+                        string deleteUserQuery = "DELETE FROM [USER] WHERE USER_ID = @userid";
+                        SqlCommand deleteUserCmd = new SqlCommand(deleteUserQuery, conn);
+                        deleteUserCmd.Parameters.AddWithValue("@userid", dataGridView1.CurrentRow.Cells["USER_ID"].Value.ToString());
+                        deleteUserCmd.ExecuteNonQuery();
+
+                        conn.Close();
+                        MessageBox.Show("User deleted successfully");
+
+                        // Refresh DataGridView by re-loading data
+                        dataGridView1.DataSource = LoadUsertable();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+        }
+    }
+}
