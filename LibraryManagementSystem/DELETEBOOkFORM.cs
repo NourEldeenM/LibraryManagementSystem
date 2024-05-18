043edc6235fa20a33506fba34e1f5f375c82883c
mariemomar <113386463+mariemomar@users.noreply.github.com>
2024-05-18 17:37:04 +0300

    Update DELETEBOOkFORM.cs

--- LibraryManagementSystem/DELETEBOOkFORM.cs
+++ LibraryManagementSystem/DELETEBOOkFORM.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp2
+namespace LibraryManagementSystem
 {
     public partial class DELETEBOOkFORM : Form
     {
