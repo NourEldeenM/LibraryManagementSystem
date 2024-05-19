f935533cf7500590bbf3d9e9bf87c965169c2cbb
mariemomar <113386463+mariemomar@users.noreply.github.com>
2024-05-19 13:57:38 +0300

    Update signupForm.cs

--- LibraryManagementSystem/signupForm.cs
+++ LibraryManagementSystem/signupForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,7 +24,7 @@ namespace LibraryManagementSystem
             InitializeComponent();
             passwordNotCorrect.Visible = false;
             passwordText.UseSystemPasswordChar = true;
-            reEnterPasswordText.UseSystemPasswordChar=true;
+            reEnterPasswordText.UseSystemPasswordChar = true;
             alert.Visible = false;
         }
 
@@ -43,6 +43,7 @@ namespace LibraryManagementSystem
         {
 
         }
+
         private string EncryptPassword(string password)
         {
             using (SHA256 sha256 = SHA256.Create())
@@ -63,133 +64,149 @@ namespace LibraryManagementSystem
             else
             {
                 alert.Visible = false;
-            
-            checkType();
 
-            if (connect.State != ConnectionState.Open)
-            {
-                connect.Open();
+                checkType();
 
-            }
-            if (connect.State == ConnectionState.Open)
-            {
-                //MessageBox.Show("opened!");
-
-                String checkUsername = "SELECT * FROM [USER] WHERE USERNAME = '"
-                            + usernameText.Text.Trim() + "'";
-
-                using (SqlCommand checkUser = new SqlCommand(checkUsername, connect))
+                if (connect.State != ConnectionState.Open)
                 {
-                    SqlDataAdapter adapter = new SqlDataAdapter(checkUser);
-                    DataTable table = new DataTable();
-                    adapter.Fill(table);
+                    connect.Open();
 
-                    if (table.Rows.Count >= 1)
-                    {
-                        MessageBox.Show(usernameText.Text + " is already exist", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                    else   //valid user name
-                    {
-
-                        string encryptedPassword = EncryptPassword(passwordText.Text.Trim());
-                        string insertData = "INSERT INTO [USER] (Email,is_admin, USERNAME, PASSWORD) VALUES (@email, @is_admin, @username, @password)";
-                        using (SqlCommand cmd = new SqlCommand(insertData, connect))
-                        {
-                            cmd.CommandType = CommandType.Text;
-                            cmd.Parameters.AddWithValue("@email", emailText.Text.Trim());
-                            cmd.Parameters.AddWithValue("@username", usernameText.Text.Trim());
-                            cmd.Parameters.AddWithValue("@password", encryptedPassword);
-                            cmd.Parameters.AddWithValue("@is_admin", isAdmin);
-                            cmd.ExecuteNonQuery();
-
-                        }
-                        //int userId = (int)cmd.ExecuteScalar();      //hena hykon fe el id
-                        if (isAdmin)
-                        {
-                            string selectUserId = "SELECT user_id FROM [USER] WHERE Email = @email AND USERNAME = @username AND PASSWORD = @password AND is_admin = 1";
-                            using (SqlCommand cmd = new SqlCommand(selectUserId, connect))
-                            {
-                                cmd.CommandType = CommandType.Text;
-                                cmd.Parameters.AddWithValue("@email", emailText.Text.Trim());
-                                cmd.Parameters.AddWithValue("@username", usernameText.Text.Trim());
-                                cmd.Parameters.AddWithValue("@password", encryptedPassword);
-                                cmd.Parameters.AddWithValue("@is_admin", isAdmin);
-                                int userId = (int)cmd.ExecuteScalar();
-                                // Now you can use the userId variable
-                                // MessageBox.Show(userId.ToString());
-
-
-                                string insertAdmin = "INSERT INTO [ADMIN] (USER_ID,is_admin,EMAIL, USERNAME, PASSWORD) VALUES (@userid,@is_admin, @email, @username, @password)";
-                                using (SqlCommand cmd1 = new SqlCommand(insertAdmin, connect))
-                                {
-                                    cmd1.CommandType = CommandType.Text;
-                                    cmd1.Parameters.AddWithValue("@userid", userId);
-                                    cmd1.Parameters.AddWithValue("@is_admin", isAdmin);
-                                    cmd1.Parameters.AddWithValue("@email", emailText.Text.Trim());
-                                    cmd1.Parameters.AddWithValue("@username", usernameText.Text.Trim());
-                                    cmd1.Parameters.AddWithValue("@password", encryptedPassword);
-                                    cmd1.ExecuteNonQuery();
-                                }
-                            }
-
-                        }
-                        else  //if it's student
-                        {
-                            string selectUserId = "SELECT user_id FROM [USER] WHERE Email = @email AND USERNAME = @username AND PASSWORD = @password AND is_admin = 0";
-                            using (SqlCommand cmd = new SqlCommand(selectUserId, connect))
-                            {
-                                cmd.CommandType = CommandType.Text;
-                                cmd.Parameters.AddWithValue("@email", emailText.Text.Trim());
-                                cmd.Parameters.AddWithValue("@username", usernameText.Text.Trim());
-                                cmd.Parameters.AddWithValue("@password", encryptedPassword);
-                                cmd.Parameters.AddWithValue("@is_admin", isAdmin);
-                                int userId = (int)cmd.ExecuteScalar();
-                                // Now you can use the userId variable
-                                //MessageBox.Show(userId.ToString());
-
-
-                                string insertAdmin = "INSERT INTO [STUDENT] (USER_ID,is_admin,EMAIL, USERNAME, PASSWORD) VALUES (@userid,@is_admin, @email, @username, @password)";
-                                using (SqlCommand cmd1 = new SqlCommand(insertAdmin, connect))
-                                {
-                                    cmd1.CommandType = CommandType.Text;
-                                    cmd1.Parameters.AddWithValue("@userid", userId);
-                                    cmd1.Parameters.AddWithValue("@is_admin", isAdmin);
-                                    cmd1.Parameters.AddWithValue("@email", emailText.Text.Trim());
-                                    cmd1.Parameters.AddWithValue("@username", usernameText.Text.Trim());
-                                    cmd1.Parameters.AddWithValue("@password", encryptedPassword);
-                                    cmd1.ExecuteNonQuery();
-                                }
-                            }
-
-
-                        }
-
-
-                        MessageBox.Show("Registered successfully", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        // Switch to the login form
-                        loginForm lForm = new loginForm();
-                        lForm.Show();
-                        this.Hide();
-
-                    }
                 }
+                if (connect.State == ConnectionState.Open)
+                {
+                    //MessageBox.Show("opened!");
 
+                    String checkUsername = "SELECT * FROM [USER] WHERE USERNAME = '"
+                                + usernameText.Text.Trim() + "'";
+
+                    using (SqlCommand checkUser = new SqlCommand(checkUsername, connect))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(checkUser);
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+                        if (table.Rows.Count >= 1)
+                        {
+                            MessageBox.Show(usernameText.Text + " is already exist", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+                    String checkEMAIL = "SELECT * FROM [USER] WHERE EMAIL = '"
+                                + emailText.Text.Trim() + "'";
+
+                    using (SqlCommand checkemail = new SqlCommand(checkEMAIL, connect))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(checkemail);
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+                        if (table.Rows.Count >= 1)
+                        {
+                            MessageBox.Show(emailText.Text + " is already exist", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+
+
+
+                    string encryptedPassword = EncryptPassword(passwordText.Text.Trim());
+                            string insertData = "INSERT INTO [USER] (Email,is_admin, USERNAME, PASSWORD) VALUES (@email, @is_admin, @username, @password)";
+                            using (SqlCommand cmd = new SqlCommand(insertData, connect))
+                            {
+                                cmd.CommandType = CommandType.Text;
+                                cmd.Parameters.AddWithValue("@email", emailText.Text.Trim());
+                                cmd.Parameters.AddWithValue("@username", usernameText.Text.Trim());
+                                cmd.Parameters.AddWithValue("@password", encryptedPassword);
+                                cmd.Parameters.AddWithValue("@is_admin", isAdmin);
+                                cmd.ExecuteNonQuery();
+
+                            }
+                            //int userId = (int)cmd.ExecuteScalar();      //hena hykon fe el id
+                            if (isAdmin)
+                            {
+                                string selectUserId = "SELECT user_id FROM [USER] WHERE Email = @email AND USERNAME = @username AND PASSWORD = @password AND is_admin = 1";
+                                using (SqlCommand cmd = new SqlCommand(selectUserId, connect))
+                                {
+                                    cmd.CommandType = CommandType.Text;
+                                    cmd.Parameters.AddWithValue("@email", emailText.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@username", usernameText.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@password", encryptedPassword);
+                                    cmd.Parameters.AddWithValue("@is_admin", isAdmin);
+                                    int userId = (int)cmd.ExecuteScalar();
+                                    // Now you can use the userId variable
+                                    // MessageBox.Show(userId.ToString());
+
+
+                                    string insertAdmin = "INSERT INTO [ADMIN] (USER_ID,is_admin,EMAIL, USERNAME, PASSWORD) VALUES (@userid,@is_admin, @email, @username, @password)";
+                                    using (SqlCommand cmd1 = new SqlCommand(insertAdmin, connect))
+                                    {
+                                        cmd1.CommandType = CommandType.Text;
+                                        cmd1.Parameters.AddWithValue("@userid", userId);
+                                        cmd1.Parameters.AddWithValue("@is_admin", isAdmin);
+                                        cmd1.Parameters.AddWithValue("@email", emailText.Text.Trim());
+                                        cmd1.Parameters.AddWithValue("@username", usernameText.Text.Trim());
+                                        cmd1.Parameters.AddWithValue("@password", encryptedPassword);
+                                        cmd1.ExecuteNonQuery();
+                                    }
+                                }
+
+                            }
+                            else  //if it's student
+                            {
+                                string selectUserId = "SELECT user_id FROM [USER] WHERE Email = @email AND USERNAME = @username AND PASSWORD = @password AND is_admin = 0";
+                                using (SqlCommand cmd = new SqlCommand(selectUserId, connect))
+                                {
+                                    cmd.CommandType = CommandType.Text;
+                                    cmd.Parameters.AddWithValue("@email", emailText.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@username", usernameText.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@password", encryptedPassword);
+                                    cmd.Parameters.AddWithValue("@is_admin", isAdmin);
+                                    int userId = (int)cmd.ExecuteScalar();
+                                    // Now you can use the userId variable
+                                    //MessageBox.Show(userId.ToString());
+
+
+                                    string insertAdmin = "INSERT INTO [STUDENT] (USER_ID,is_admin,EMAIL, USERNAME, PASSWORD) VALUES (@userid,@is_admin, @email, @username, @password)";
+                                    using (SqlCommand cmd1 = new SqlCommand(insertAdmin, connect))
+                                    {
+                                        cmd1.CommandType = CommandType.Text;
+                                        cmd1.Parameters.AddWithValue("@userid", userId);
+                                        cmd1.Parameters.AddWithValue("@is_admin", isAdmin);
+                                        cmd1.Parameters.AddWithValue("@email", emailText.Text.Trim());
+                                        cmd1.Parameters.AddWithValue("@username", usernameText.Text.Trim());
+                                        cmd1.Parameters.AddWithValue("@password", encryptedPassword);
+                                        cmd1.ExecuteNonQuery();
+                                    }
+                                }
+
+
+                            }
+
+
+                            MessageBox.Show("Registered successfully", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            // Switch to the login form
+                            loginForm lForm = new loginForm();
+                            lForm.Show();
+                            this.Hide();
+
+                        
+                    
+
+                }
             }
+
         }
-            
-        }
-        private void passwordNotCorrect_Click(object sender , EventArgs e)
+        private void passwordNotCorrect_Click(object sender, EventArgs e)
         {
 
         }
 
         private void checkBoxPass2_CheckedChanged(object sender, EventArgs e)
         {
-            if (checkBoxPass2.Checked == false) {
-                passwordText.UseSystemPasswordChar = true; 
+            if (checkBoxPass2.Checked == false)
+            {
+                passwordText.UseSystemPasswordChar = true;
                 reEnterPasswordText.UseSystemPasswordChar = true;
-            } 
-            else 
+            }
+            else
             {
                 passwordText.UseSystemPasswordChar = false;
                 reEnterPasswordText.UseSystemPasswordChar = false;
@@ -243,7 +260,20 @@ namespace LibraryManagementSystem
             }
         }
 
+        private void signupForm_Load(object sender, EventArgs e)
+        {
 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void emailText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     } // end of the signup functionalities
 }
 
@@ -255,59 +285,7 @@ namespace LibraryManagementSystem
 
 
 
-// function to determine if it's student or admin                    (done)
-// function to set the data in USER table                             (done)
-// function to put the data in the correct table "admin or student"     (done)
-// function to check username is not repeated                           (done)
-// function to check if all the fields are filled or not              (done)
-// encryption the password                                             (done)
 
 
 
 
-
-
-
-
-
-
-
-
-//=================================================================================================================================
-
-
-/*
-  int userId = (int)cmd.ExecuteScalar();      //hena hykon fe el id
-                            if (isAdmin)
-                            {
-                                string insertDataAdmin = "INSERT INTO [ADMIN] (USER_ID, is_admin, EMAIL,USERNAME , PASSWORD) VALUES (@userId, @is_admin, @email,@username, @password)";
-                                using (SqlCommand cmdAdmin = new SqlCommand(insertDataAdmin, connect))
-                                {
-                                    cmdAdmin.CommandType = CommandType.Text;
-                                    cmdAdmin.Parameters.AddWithValue("@userId", userId);
-                                    cmdAdmin.Parameters.AddWithValue("@username", usernameText.Text.Trim());
-                                    cmdAdmin.Parameters.AddWithValue("@password", passwordText.Text.Trim());
-                                    cmdAdmin.Parameters.AddWithValue("@email", emailText.Text.Trim());
-                                    cmdAdmin.Parameters.AddWithValue("@is_admin", isAdmin);
-
-
-                                    cmdAdmin.ExecuteNonQuery();
-                                }
-                            }
-                            else
-                            {
-                                string insertDataStudent = "INSERT INTO [STUDENT] (USER_ID, is_admin, EMAIL,USERNAME , PASSWORD) VALUES (@userId, @is_admin, @email,@username, @password)";
-                                using (SqlCommand cmdStudent = new SqlCommand(insertDataStudent, connect))
-                                {
-                                    cmdStudent.CommandType = CommandType.Text;
-                                    cmdStudent.Parameters.AddWithValue("@userId", userId);
-                                    cmdStudent.Parameters.AddWithValue("@email", emailText.Text.Trim());
-                                    cmdStudent.Parameters.AddWithValue("@username", usernameText.Text.Trim());
-                                    cmdStudent.Parameters.AddWithValue("@password", passwordText.Text.Trim());
-                                    cmdStudent.Parameters.AddWithValue("@is_admin", isAdmin);
-
-                                    cmdStudent.ExecuteNonQuery();
-                                }
-                            }
- 
- */
